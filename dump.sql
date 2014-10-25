a472947a38f6d8eb3f790018d6fbbe811da612c9
Mark Hähnel <mark.haehnel@ish.de>
2014-10-25 12:14:40 +0200

    Added DB dump, small file modifications

---
+++ dump.sql
@@ -0,0 +1,32 @@
+CREATE TABLE IF NOT EXISTS `accounts` (
+  `userid` int(11) NOT NULL AUTO_INCREMENT,
+  `username` text NOT NULL,
+  `nickname` text NOT NULL,
+  `password` text NOT NULL,
+  `email` text NOT NULL,
+  `role` int(11) NOT NULL,
+  `lastlogin` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `lastip` text NOT NULL,
+  PRIMARY KEY (`userid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+CREATE TABLE IF NOT EXISTS `blogposts` (
+  `postid` int(11) NOT NULL AUTO_INCREMENT,
+  `title` text NOT NULL,
+  `content` text NOT NULL,
+  `userid` int(11) NOT NULL,
+  `category` int(11) NOT NULL,
+  PRIMARY KEY (`postid`),
+  KEY `userid` (`userid`),
+  KEY `category` (`category`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+CREATE TABLE IF NOT EXISTS `categories` (
+  `catid` int(11) NOT NULL AUTO_INCREMENT,
+  `title` text NOT NULL,
+  PRIMARY KEY (`catid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+ALTER TABLE `blogposts`
+  ADD CONSTRAINT `blogposts_ibfk_2` FOREIGN KEY (`userid`) REFERENCES `accounts` (`userid`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `blogposts_ibfk_1` FOREIGN KEY (`category`) REFERENCES `categories` (`catid`) ON DELETE CASCADE ON UPDATE CASCADE;
--- index.php
+++ index.php
@@ -1,8 +1,12 @@
+<?php
+	
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Document</title>
+	<title>slant-cms</title>
 </head>
 <body>
 	
