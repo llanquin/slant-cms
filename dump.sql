e0d19d3fb521d59991996efe6d4d062fc509dfa5
Mark Hähnel <mark.haehnel@ish.de>
2014-10-25 14:58:43 +0200

    Corrected sql dump

--- dump.sql
+++ dump.sql
@@ -1,33 +1,27 @@
 CREATE TABLE IF NOT EXISTS `accounts` (
-  `userid` int(11) NOT NULL AUTO_INCREMENT,
-  `username` text NOT NULL,
-  `nickname` text NOT NULL,
-  `password` text NOT NULL,
-  `email` text NOT NULL,
-  `role` int(11) NOT NULL,
-  `lastlogin` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `lastip` text NOT NULL,
-  PRIMARY KEY (`userid`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  `userid` INT NOT NULL AUTO_INCREMENT,
+  `username` TEXT NOT NULL,
+  `password` TEXT NOT NULL,
+  `email` TEXT NOT NULL,
+  `displayname` TEXT NOT NULL,
+  `role` INT NOT NULL,
+  `lastlogin` TIMESTAMP NOT NULL,
+  `lastip` TEXT NOT NULL,
+  PRIMARY KEY (`userid`))
+ENGINE = MyISAM;
 
 CREATE TABLE IF NOT EXISTS `blogposts` (
-  `postid` int(11) NOT NULL AUTO_INCREMENT,
-  `title` text NOT NULL,
-  `content` text NOT NULL,
-  `userid` int(11) NOT NULL,
-  `datetime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-  `category` int(11) NOT NULL,
-  PRIMARY KEY (`postid`),
-  KEY `userid` (`userid`),
-  KEY `category` (`category`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  `postid` INT NOT NULL AUTO_INCREMENT,
+  `title` TEXT NOT NULL,
+  `content` TEXT NOT NULL,
+  `userid` INT NOT NULL,
+  `datetime` TIMESTAMP NOT NULL,
+  `category` INT NOT NULL,
+  PRIMARY KEY (`postid`))
+ENGINE = MyISAM;
 
 CREATE TABLE IF NOT EXISTS `categories` (
-  `catid` int(11) NOT NULL AUTO_INCREMENT,
-  `title` text NOT NULL,
-  PRIMARY KEY (`catid`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
-ALTER TABLE `blogposts`
-  ADD CONSTRAINT `blogposts_ibfk_2` FOREIGN KEY (`userid`) REFERENCES `accounts` (`userid`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `blogposts_ibfk_1` FOREIGN KEY (`category`) REFERENCES `categories` (`catid`) ON DELETE CASCADE ON UPDATE CASCADE;
+  `catid` INT NOT NULL AUTO_INCREMENT,
+  `categoryname` TEXT NOT NULL,
+  PRIMARY KEY (`catid`))
+ENGINE = MyISAM;
