4588fe9967de76206045f08f70e3210dc1e6bbfc
Mark Hähnel <mark.haehnel@ish.de>
2014-10-26 09:17:50 +0100

    Bootstrap and basic config added

---
+++ _config.php
@@ -0,0 +1,26 @@
+<?php
+	
+	/* slant-cms Config File
+	========================
+	This is the central configiguration file.
+	Everything that's changeable is here. */
+	
+	
+	/* MySQL connection
+	=================== */
+	$connectionParams = [
+		"hostname"		=> "",
+		"database"		=> "",
+		"username"		=> "",
+		"password"		=> "",
+	];
+
+	/* General settings
+	=================== */
+	$settingsGeneral = [
+		"title"			=> "",
+		"subtitle"		=> "",
+		"canRegister"	=> "",
+	];
+
+?>
