2c122fc76c045484a01bfad2a0b7d0f232e1ebe4
Dominik <dominik.sudol1992@gmail.com>
2014-10-26 16:31:42 +0100

    Added function getBlogPosts and changed mysql_querymethod in getUser()
    Part of #4

--- inc/global.php
+++ inc/global.php
@@ -1,13 +1,14 @@
 <?php 
-   function getUser() {
-       $array = array();
+
+	/* Array of all users data that exist in the database */
+   function getUsers() {
        $sql = "SELECT * FROM accounts";
        $ressource = mysql_query($sql); 
        
 	   
 	   $counter = 0;
        while($row = mysqli_fetch_assoc($ressource)) {
-           $array[$counter] = array(
+           $array[$counter] = [
 			   'userid' => $row['userid'],
 			   'username' => $row['username'],
 			   'password' => $row['password'],
@@ -16,9 +17,37 @@
 			   'role' => $row['role'],
 			   'lastlogin' => $row['lastlogin'],
 			   'lastip' => $row['lastip']
-           )
-			   $counter++;
+		   ];
+		   $counter++;
        }
 	   return $array;
    } 
+	
+	/* Array of a single user data */
+	function getUser($id) {
+       $sql = "SELECT * FROM accounts WHERE userid = " . $id;
+       $ressource = mysql_query($sql); 
+	   return $ressource;
+	}
+	
+	/* Array of all blogposts in the database */
+	function getBlogPost() {
+
+		$sql = "SELECT * FROM blogposts";
+		$ressource = mysql_query($sql);
+		
+		$counter = 0;
+		while($row = mysqli_fetch_assoc($ressource)) {
+           $array[$counter] = [
+			   'postid' => $row['postid'],
+			   'title' => $row['title'],
+			   'content' => $row['content'],
+			   'userid' => $row['userid'],
+			   'datetime' => $row['datetime'],
+			   'category' => $row['category']
+		   ];
+		   $counter++;
+       }
+	   return $array;
+	}
 ?>
