73b7c9ed647acb4848433d0959bd7318c33e8422
Mark Hähnel <EZTEQ@users.noreply.github.com>
2014-10-27 15:32:16 +0100

    Updated Blogpost Layout
    Part of #2.

--- templates/home.php
+++ templates/home.php
@@ -1,14 +1,17 @@
 <?php $posts = getBlogPosts($con); ?>
 
 <div class="container">
-	<?php 
-		foreach($posts as $post) {
-			echo('<div class="row">');
-			
-			echo('<h1>' . $post['title'] . '</h1>');
-			echo('<p>' . $post['content'] . '</p>');
-			
-			echo('</div>');
-		}
-	?>
+	<?php foreach($posts as $post) : ?>
+		<div class="panel panel-default">
+			<div class="panel-heading"><h1><?php $post['title']; ?></h1></div>
+			<div class="panel-body">
+				<?php $post['content']; ?>
+			</div>
+			<div class="panel-footer">
+				<p class="text-right">
+					<?php date("H:m:s d-m-Y", $post['datetime']); ?> &ndash; <a href="#"><?php getUser($con, $post['userid']); ?></a>
+				</p>
+			</div>
+		</div>
+	<?php endforeach ?>
 </div>
